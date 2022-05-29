03de0d2a2b1457973a9bde8872a6680ef10824b7
René Hansen <renehh@gmail.com>
2022-05-29 17:46:07 +0200

    Initial commit
    This adds the base cookiecutter configuration and the bare minimum files
    needed for a docker compose based development environment.

---
+++ {{cookiecutter.directory_name}}/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# -e  Exit immediately if a command exits with a non-zero status.
+# -u  Treat unset variables as an error when substituting.
+# -x  Print commands and their arguments as they are executed.
+# -o option-name
+#   pipefail     the return value of a pipeline is the status of
+#                the last command to exit with a non-zero status,
+set -euxo pipefail
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /{{cookiecutter.directory_name}}/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
