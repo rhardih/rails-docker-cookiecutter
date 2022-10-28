dce5a051e5e88595997a0cd16261d36a8ab92a09
René Hansen <renehh@gmail.com>
2022-10-28 09:53:04 +0200

    Set config.hosts in fix template

--- {{cookiecutter.project_slug}}/post-gen-fixes.rb
+++ {{cookiecutter.project_slug}}/post-gen-fixes.rb
@@ -9,3 +9,12 @@ inject_into_file 'config/database.yml', after: "pool: <%= ENV.fetch(\"RAILS_MAX_
   password: password
   YML
 end
+
+# Set hosts to allow requests in a production deploy
+inject_into_file 'config/environments/production.rb',
+ after: "config.active_record.dump_schema_after_migration = false\n" do  <<-'RUBY'
+
+  # Allow requests through the ActionDispatch::HostAuthorization middleware
+  config.hosts = ["{{cookiecutter.domain}}"]
+  RUBY
+end
